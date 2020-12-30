6a621f96e6c01b7adb5fe88aba17786ab622a7e1
Helen Cunningham <hkathleenc@gmail.com>
2020-12-29 16:59:00 -0800

    Added some admin files (package.json, package-lock.json, .gitignore. Also added a __tests__ folder and PageObjects folder. Added a base class with code from previous assignments that will perform basic navigation/clicking and input functions. Added an empty SmartLinkPage class.

---
+++ __tests__/SmartLinkPage.ts
@@ -0,0 +1,12 @@
+import {
+    Builder,
+    By,
+    Capabilities,
+    until,
+    WebDriver,
+  } from "selenium-webdriver";
+  import { BasePage } from "./BasePage";
+
+  export class SmartLinkPage extends BasePage {
+      
+  }
